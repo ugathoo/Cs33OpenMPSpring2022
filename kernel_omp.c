f9230828bdc387e6f0517c8d2b820868277c1f19
ugathoo <106710857+ugathoo@users.noreply.github.com>
2022-06-01 15:52:08 -0700

    Add files via upload

---
+++ kernel_omp.c
@@ -0,0 +1,103 @@
+#include <omp.h>
+
+#include "kernel.h"
+
+void kernel_omp(int *input, int *ref, int64_t rows, int64_t cols, int penalty) {
+  /* int i, j; 
+
+#pragma omp parallel for private (j)
+for(int i = 1; i < rows; i++){
+    for(int j = 1; j <= i; j++){
+      int64_t idx = i * cols + j;                                                                                                                                                
+      int sub = idx -cols;                                                                                                                                                       
+      int64_t idxNW = sub - 1;                                                                                                                                                   
+      int64_t idxN = sub;                                                                                                                                                        
+      int64_t idxW = idx - 1;                                                                                                                                                    
+      int r = ref[idx];                                                                                                                                                          
+      int inputNW = input[idxNW];                                                                                                                                                
+      int inputW = input[idxW];                                                                                                                                                  
+      int inputN = input[idxN];                                                                                                                                                                                                                                                                           input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);   
+    }
+ }
+
+#pragma omp barrier
+#pragma omp parallel for private(i)
+  for(int j = cols -1; j > 0; j--){
+    for(int i = 2; i < j; i++){
+      int64_t idx = i * cols + j;                                                                                                                                                
+      int sub = idx -cols;                                                                                                                                                       
+      int64_t idxNW = sub - 1;                                                                                                                                                   
+      int64_t idxN = sub;                                                                                                                                                        
+      int64_t idxW = idx - 1;                                                                                                                                                    
+      int r = ref[idx];                                                                                                                                                          
+      int inputNW = input[idxNW];                                                                                                                                                
+      int inputW = input[idxW];                                                                                                                                                  
+      int inputN = input[idxN];                                                                                                                                                  
+                                                                                                                                                                                 
+      input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);   
+    }
+  }
+
+
+}*/ 
+
+ int TILESIZE = 4;
+ int j, initialI;
+#pragma omp parallel for private(j, initialI) 
+  for(int i = 1; i < rows-TILESIZE; i += TILESIZE){
+    for(initialI = i; initialI > 0; initialI--){
+      for(j = 1; j <= initialI; j++){
+	for (int k = i; k < i + TILESIZE; k++){
+	  for(int l = j; l < j + TILESIZE; l++){
+	    int64_t idx = k * cols + l;                                                       
+                     
+	    int64_t idxNW = idx - cols - 1;                                                  
+	    int64_t idxN = idx - cols;                                                       
+	    int64_t idxW = idx - 1;                                                          
+	    int r = ref[idx];                                                                
+	    int inputNW = input[idxNW];                                                      
+	    int inputW = input[idxW];                                                        
+	    int inputN = input[idxN];                                                         
+	    input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty); 
+	  }
+	}
+      }
+    }
+  }
+}
+/*#pragma omp parallel for collapse(3)
+ for (int i = 1; i < rows; i++) {
+    for(int initialI = i; initialI > 0; initialI--){
+      for(int j = 1; j <= initialI; j++){
+      int64_t idx = i * cols + j;
+      int sub = idx -cols;
+      int64_t idxNW = sub - 1;
+      int64_t idxN = sub;
+      int64_t idxW = idx - 1;
+      int r = ref[idx];
+      int inputNW = input[idxNW];
+      int inputW = input[idxW];
+      int inputN = input[idxN];
+
+      input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
+      }
+    }
+ }
+}*/
+ /* #pragma parallel for collapse(2)
+  for (int i = 1; i < rows; ++i) {
+    for (int j = 1; j < cols; ++j) {
+      int64_t idx = i * cols + j;
+      int64_t temp = idx - cols;
+      int64_t idxNW = temp - 1;
+      int64_t idxN = temp;
+      int64_t idxW = idx - 1;
+      int r = ref[idx];
+      int inputNW = input[idxNW];
+      int inputW = input[idxW];
+      int inputN = input[idxN];
+
+      input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
+    }
+    }
+ }*/
